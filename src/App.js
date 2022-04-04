5def7e6ea54706e71d229fec8f27b9f1e830181a
mjstrz <strzelecki.monica7@gmail.com>
2022-04-03 23:44:39 -0400

    Basic page setup - headers, background, font styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
+
+  //Requirements: 
+
+  //Info: Photo, name, email and linkedIn buttons
+  //About
+  // Interests
+  // Footer (social icons) i.e. github button 
+  // can implement a light mode/dark mode (see React Info page)
+
+  // https://www.figma.com/file/4ctPLUvIn5b5Ep6YPOZWWd/Digital-Business-Card?node-id=0%3A1 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Monica Strzelecki</h1>
+        <p>Frontend Developer</p>
+        <Button className="button email">Email</Button>
+        <Button className="button linkedin">LinkedIn</Button>
+        <h2>About</h2>
+        <h2>Interests</h2>
     </div>
   );
 }
