1362add672fce85d8102f7931a059ff0e80cd402
mjstrz <strzelecki.monica7@gmail.com>
2022-05-31 21:18:20 -0400

    copyright footer

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,7 +14,7 @@ export default function Main(props) {
             <div className='buttons'>
                 <ul className='social-media-buttons'>
                     <li><a role="button" href="mailto: strzelecki.monica7@gmail.com"><i className='fa fa-envelope'></i></a></li>
-                    <li><a className="btn btn-social-icon btn-linkedin" href="https://www.linkedin.com/in/monica-strzelecki/" target="_blank"> <i classname="fa fa-linkedin"></i></a>{' '}</li>
+                    <li><a className="btn btn-social-icon btn-linkedin" href="https://www.linkedin.com/in/monica-strzelecki/" target="_blank"> <i class="bi bi-linkedin"></i></a>{' '}</li>
                     <li><a className="btn btn-social-icon btn-github" href="https://www.github.com/mjstrz" target="_blank"><i className='fa fa-github'></i></a> </li>
                 </ul>                 
             </div>
@@ -24,6 +24,10 @@ export default function Main(props) {
                 <p>8+ years of professional experience in Higher Education in 2 countries. Detail-oriented and an avid learner. </p>
             <h2 className='main--header'>Interests</h2>
                 <p>Being outside. Skiing. Biking. Electronic music. Porch Gardening. Building React sites!</p>
+
+                <footer className='site-footer'>      
+                    <h5> Copyright &copy; 2021 Monica Strzelecki </h5>
+                </footer>
     </main>
     )
 }
