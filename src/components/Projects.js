34dd40d6e50cf92cefec9342cd9faa86f6036e3a
mjstrz <strzelecki.monica7@gmail.com>
2023-04-16 23:56:12 -0400

    Projects - changed photo alts and card groupings

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardDeck, CardTitle, Row, Col, CardBody, CardText, CardGroup, CardSubtitle, Button } from 'reactstrap';
-import { Link } from 'react-router-dom';
-import farineLogo from '../images/Farine_logo2.png';
-import memeLogo from '../images/memeEmoji.jpg';
-import blackJack from '../images/Blackjack-21_v2.jpg';
-import tenzies from '../images/tenziesDice.jpg';
-import Lavender from '../images/lavender/lavenderFieldParallax.jpg'
+import React from "react";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardDeck,
+  CardTitle,
+  Row,
+  Col,
+  CardBody,
+  CardText,
+  CardGroup,
+  CardSubtitle,
+  Button,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import farineLogo from "../images/Farine_logo2.png";
+import memeLogo from "../images/memeEmoji.jpg";
+import blackJack from "../images/Blackjack-21_v2.jpg";
+import tenzies from "../images/tenziesDice.jpg";
+import Lavender from "../images/lavender/lavenderFieldParallax.jpg";
 
-
-// adds scrolling animation 
+// adds scrolling animation
 function reveal() {
   const reveals = document.querySelectorAll(".reveal");
   for (let i = 0; i < reveals.length; i++) {
@@ -28,159 +40,193 @@ window.addEventListener("scroll", reveal);
 // To check the scroll position on page load
 reveal();
 
-export default function Projects() { 
-
-    
-    return (
-      <section className="projects-section" id="projects">       
-        <div className="projects">
-        <h2 id="projects-header" className='reveal'>Projects</h2>
-            {/* <div class="row projects-header-divider"> */}
-          {/* <hr class="divider divider-light" /> */}
+export default function Projects() {
+  return (
+    <section className="projects-section" id="projects">
+      <div className="projects">
+        <h2 id="projects-header" className="reveal">
+          Projects
+        </h2>
+        {/* <div class="row projects-header-divider"> */}
+        {/* <hr class="divider divider-light" /> */}
         {/* </div> */}
 
-          <CardGroup>
+        <CardGroup>
           {/* <row> */}
-            {/* Card #1 */}
-            <Card className="col-md-4 reveal">
-              <CardImg 
-                classname="farine-logo"
-                alt="Farine cafe logo"
-                src={farineLogo}
-                top
-                width="33%"
-              />
-              <CardBody>
-                <CardTitle tag="h5">Farine Bakery & Cat Cafe</CardTitle>
-                <CardSubtitle
-                  className="mb-2 text-muted align-items-center"
-                  tag="h6"
-                ></CardSubtitle>
-                <CardText>
-                  A cozy and quaint cat cafe site I created for a NuCamp portfolio assignment, built using React JS.
-                </CardText>
-                <Button className="github-button" target="_blank" href="https://mjstrz.github.io/farine-react/">Live Site</Button>
-                <Button className="github-button" target="_blank" href="https://github.com/mjstrz/farine-react">Code</Button>
-              </CardBody>
-            </Card>
+          {/* Card #1 */}
+          <Card className="col-md-4 reveal">
+            <CardImg
+              classname="farine-logo"
+              alt="Farine cafe logo"
+              src={farineLogo}
+              top
+              width="33%"
+            />
+            <CardBody>
+              <CardTitle tag="h5">Farine Bakery & Cat Cafe</CardTitle>
+              <CardSubtitle
+                className="mb-2 text-muted align-items-center"
+                tag="h6"
+              ></CardSubtitle>
+              <CardText>
+                A cozy and quaint cat cafe site I created for a NuCamp portfolio
+                assignment, built using React JS.
+              </CardText>
+              <Button
+                className="github-button"
+                target="_blank"
+                href="https://mjstrz.github.io/farine-react/"
+              >
+                Live Site
+              </Button>
+              <Button
+                className="github-button"
+                target="_blank"
+                href="https://github.com/mjstrz/farine-react"
+              >
+                Code
+              </Button>
+            </CardBody>
+          </Card>
 
-            {/*Card #2  */}
-            <Card className="col-md-4 reveal">
-              <CardImg
-                alt="Card image cap"
-                src={Lavender}
-                top
-                width="%"
-              />
-              <CardBody>
-                <CardTitle tag="h5">This Portfolio Site</CardTitle>
-                {/* <CardSubtitle className="mb-2 text-muted" tag="h6">
+          {/*Card #2  */}
+          <Card className="col-md-4 reveal">
+            <CardImg alt="Lavender field" src={Lavender} top width="%" />
+            <CardBody>
+              <CardTitle tag="h5">This Portfolio Site</CardTitle>
+              {/* <CardSubtitle className="mb-2 text-muted" tag="h6">
                   Card subtitle
                 </CardSubtitle> */}
-                <CardText>
-                  Feel free to check out my code for making this portfolio site come to life, created using React JS. 
-                </CardText>
-                {/* <Button className="github-button" href="https://mjstrz.github.io/meme_generator/">Live Site</Button> */}
-                <Button className="github-button" target="_blank" href="https://github.com/mjstrz/about_me">Code</Button>
-              </CardBody>
-            </Card>
-            </CardGroup>
-            
-            <CardGroup>
-            <Card className="col-md-4 col-1 reveal">
-              <CardImg
-                alt="Card image cap"
-                src={farineLogo}
-                top
-                width="33%"
-              />
-              <CardBody>
-                <CardTitle tag="h5">Farine Bakery & Cafe [Bootstrap]</CardTitle>
-                {/* <CardSubtitle className="mb-2 text-muted" tag="h6">
+              <CardText>
+                Feel free to check out my code for making this portfolio site
+                come to life, created using React JS.
+              </CardText>
+              {/* <Button className="github-button" href="https://mjstrz.github.io/meme_generator/">Live Site</Button> */}
+              <Button
+                className="github-button"
+                target="_blank"
+                href="https://github.com/mjstrz/about_me"
+              >
+                Code
+              </Button>
+            </CardBody>
+          </Card>
+        </CardGroup>
+
+        <CardGroup>
+          <Card className="col-md-4 mt-5 card-bottom reveal">
+            <CardImg
+              alt="Farine cat cafe logo"
+              src={farineLogo}
+              top
+              width="33%"
+            />
+            <CardBody>
+              <CardTitle tag="h5">Farine Bakery & Cafe [Bootstrap]</CardTitle>
+              {/* <CardSubtitle className="mb-2 text-muted" tag="h6">
                   Card subtitle
                 </CardSubtitle> */}
-                <CardText>
-                  My cat cafe NuCamp Portfolio assignment that I initially created using Bootstrap.
-                </CardText>
-                <Button className="github-button" target="_blank" >Live Site</Button>
-                <Button className="github-button" target="_blank" href="https://github.com/mjstrz/farine-bootstrap-updated">Code</Button>
-              </CardBody>
-            </Card>
+              <CardText>
+                My cat cafe NuCamp Portfolio assignment that I initially created
+                using Bootstrap.
+              </CardText>
+              <Button className="github-button" target="_blank">
+                Live Site
+              </Button>
+              <Button
+                className="github-button"
+                target="_blank"
+                href="https://github.com/mjstrz/farine-bootstrap-updated"
+              >
+                Code
+              </Button>
+            </CardBody>
+          </Card>
 
           {/* </CardGroup> */}
 
           {/* <CardGroup> */}
-            {/* Card #3 */}
-            <Card className="col-md-4 col-1 card-bottom reveal">
-              <CardImg
-                alt="Card image cap"
-                src={memeLogo}
-                top
-                width="33%"
-              />
-              <CardBody>
-                <CardTitle tag="h5">Meme Generator</CardTitle>
-                {/* <CardSubtitle className="mb-2 text-muted" tag="h6">
+          {/* Card #3 */}
+          <Card className="col-md-4 mt-5 card-bottom reveal">
+            <CardImg alt="Three different emoji faces" src={memeLogo} top width="33%" />
+            <CardBody>
+              <CardTitle tag="h5">Meme Generator</CardTitle>
+              {/* <CardSubtitle className="mb-2 text-muted" tag="h6">
                   Card subtitle
                 </CardSubtitle> */}
-                <CardText>
-                  A fun meme generator built using React JS. 
-                </CardText>
-                <Button className="github-button" target="_blank" href="https://mjstrz.github.io/meme_generator/">Live Site</Button>
-                <Button className="github-button" target="_blank" href="https://github.com/mjstrz/meme_generator">Code</Button>
-              </CardBody>
-            </Card>
-            </CardGroup>
+              <CardText>A fun meme generator built using React JS.</CardText>
+              <Button
+                className="github-button"
+                target="_blank"
+                href="https://mjstrz.github.io/meme_generator/"
+              >
+                Live Site
+              </Button>
+              <Button
+                className="github-button"
+                target="_blank"
+                href="https://github.com/mjstrz/meme_generator"
+              >
+                Code
+              </Button>
+            </CardBody>
+          </Card>
+        </CardGroup>
 
-            {/* Card #4 */}
-            
+        {/* Card #4 */}
 
-            {/* Card #5 */}
-            <CardGroup>
-            <Card className="col-md-4 col-1 card-bottom reveal">
-              <CardImg
-                alt="Card image cap"
-                src={tenzies}
-                top
-                width="33%"
-              />
-              <CardBody>
-                <CardTitle tag="h5">Tenzies</CardTitle>
-                {/* <CardSubtitle className="mb-2 text-muted" tag="h6">
+        {/* Card #5 */}
+        <CardGroup>
+          <Card className="col-md-4 card-bottom reveal">
+            <CardImg alt="6 die on a black tabletop" src={tenzies} top width="33%" />
+            <CardBody>
+              <CardTitle tag="h5">Tenzies</CardTitle>
+              {/* <CardSubtitle className="mb-2 text-muted" tag="h6">
                   Card subtitle
                 </CardSubtitle> */}
-                <CardText>
-                  A fun game designed using React JS. 
-                </CardText>
-                <Button className="github-button" target="_blank" href="https://mjstrz.github.io/tenzies_game/">Live Site</Button>
-                <Button className="github-button" target="_blank" href="https://github.com/mjstrz/tenzies_game">Code</Button>
-              </CardBody>
-            </Card>
+              <CardText>A fun game designed using React JS.</CardText>
+              <Button
+                className="github-button"
+                target="_blank"
+                href="https://mjstrz.github.io/tenzies_game/"
+              >
+                Live Site
+              </Button>
+              <Button
+                className="github-button"
+                target="_blank"
+                href="https://github.com/mjstrz/tenzies_game"
+              >
+                Code
+              </Button>
+            </CardBody>
+          </Card>
 
-            {/* Card #6 */}
+          {/* Card #6 */}
 
-            <Card className="col-md-4 col-1 card-bottom reveal">
-              <CardImg
-                alt="Card image cap"
-                src={blackJack}
-                top
-                width="33%"
-              />
-              <CardBody>
-                <CardTitle tag="h5">Black Jack</CardTitle>
-                {/* <CardSubtitle className="mb-2 text-muted" tag="h6">
+          <Card className="col-md-4 col-1 card-bottom reveal">
+            <CardImg alt="Peeking at cards on a blackjack table" src={blackJack} top width="33%" />
+            <CardBody>
+              <CardTitle tag="h5">Black Jack</CardTitle>
+              {/* <CardSubtitle className="mb-2 text-muted" tag="h6">
                   Card subtitle
                 </CardSubtitle> */}
-                <CardText>
-                  A fun game designed using React JS. 
-                </CardText>
-                <Button className="github-button" target="_blank">Live Site</Button>
-                <Button className="github-button" target="_blank" href="https://github.com/mjstrz/blackjack_game">Code</Button>
-              </CardBody>
-            </Card>
-            {/* </row> */}
-          </CardGroup>
-        </div>
-      </section>
-    );
+              <CardText>A fun game designed using React JS.</CardText>
+              <Button className="github-button" target="_blank">
+                Live Site
+              </Button>
+              <Button
+                className="github-button"
+                target="_blank"
+                href="https://github.com/mjstrz/blackjack_game"
+              >
+                Code
+              </Button>
+            </CardBody>
+          </Card>
+          {/* </row> */}
+        </CardGroup>
+      </div>
+    </section>
+  );
 }
